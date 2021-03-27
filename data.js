a9350156b331a925d960572fa12fb04af5b7711d
srikanth <kasireddysrikanth27@gmail.com>
2021-03-27 12:10:04 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,26 @@
+
+  
+var data = {
+  name : 'srikanth kasireddy',
+  bio: [
+    {name: 'Name', value: 'srikanth kasireddy'},
+    {name: 'Email', value: 'kasireddysrikanth27@gmail.com'},
+    {name: 'Phone', value: '9848282503'},
+    {name: 'Country', value: 'India'},
+    {name: 'City', value: 'Hyderabad'}
+  ],
+  github: 'srikanth-11',
+  projects: [
+    {name: 'Questionsoverflow', url: 'https://sri-questionsoverflow.netlify.app'},
+    {name: 'Uploader', url: 'https://sri-uploader.netlify.app'},
+    {name: 'Socialize', url: 'https://sri-socialize.netlify.app'},
+    {name: 'Tasksubmission', url: 'https://sri-tasksubmission.netlify.app'},
+  ],
+  resume: 'https://sri-resume.netlify.app',
+  socials: [
+    {name: 'LinkedIn', url: 'https://linkedin.com/in/srikanth-11'},
+    {name: 'GitHub', url: 'https://github.com/srikanth-11'},
+    
+  ]
+}
+
